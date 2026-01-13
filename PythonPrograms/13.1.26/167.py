c4345a64fad187b3a8a1b54f1f60596f03bac18c
Yuvi <yuvasmail1@gmail.com>
2026-01-13 16:58:03 +0530

    One more 	modified:   PythonPrograms/13.1.26/167.py

--- PythonPrograms/13.1.26/167.py
+++ PythonPrograms/13.1.26/167.py
@@ -2,4 +2,12 @@
 #! smallest power of 2 greater than or equal to n
 
 def calculate_smallest_power_of_2(n):
-    return 0
+    power_of_two = 2
+    while not power_of_two >= n:
+        power_of_two *= 2
+
+
+
+    return power_of_two
+
+print(calculate_smallest_power_of_2(64))
